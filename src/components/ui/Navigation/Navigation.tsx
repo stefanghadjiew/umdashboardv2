82e89396a6741f892c9dabea26788d007e6dffb9
Stefan <shadzhiev@eservicios.indra.es>
2025-08-17 01:23:35 +0300

    add avatar back

--- src/components/ui/Navigation/Navigation.tsx
+++ src/components/ui/Navigation/Navigation.tsx
@@ -6,19 +6,15 @@ import { Avatar, AvatarFallback, AvatarImage } from "@shadcn-components"
 
 
 export const Navigation = () => {
-    const { data, isPending } = useSession();
-
-    if(isPending) {
-        return <div>Loading session...</div>
-    }
+    const { data } = useSession();
 
     return (
         <div className="fixed w-full max-w-[500px] flex justify-between mx-2 px-8 pt-2">
             <LogoutButton />
-            {/* <Avatar>
+            <Avatar>
                 <AvatarImage src={data?.user?.image ?? ''} />
                 <AvatarFallback>CN</AvatarFallback>
-            </Avatar> */}
+            </Avatar>
         </div>
     )
 }
