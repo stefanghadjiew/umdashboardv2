ac8d11c6f07e9e0a7fbfcb5e660572cd2a4e5dcd
Stefan <shadzhiev@eservicios.indra.es>
2025-08-16 23:10:57 +0300

    fix more TS errors

--- src/components/ui/ChampionCard/ChampionCard.tsx
+++ src/components/ui/ChampionCard/ChampionCard.tsx
@@ -1,9 +1,8 @@
 import { resources } from "../../../../public/champions";
-import { IChampion } from "app/intefaces";
 
 interface IChampionCard {
     name: string;
-    onClick: (champion: IChampion) => void;
+    onClick: () => void;
     className?: string;
 }
 
@@ -11,6 +10,6 @@ export const ChampionCard = ({name, onClick, className} : IChampionCard) => {
     const image = resources[name] ?? '';
     const componentClasses = `inline-block h-[200px] w-[120px] bg-cover bg-center cursor-pointer text-red ${className}`;
     return (
-        <div onClick={onClick} className={componentClasses} style={{ backgroundImage: `url(${image.src})` }}>{name}</div>
+        <div onClick={() => onClick()} className={componentClasses} style={{ backgroundImage: `url(${image.src})` }}>{name}</div>
     )
 }
