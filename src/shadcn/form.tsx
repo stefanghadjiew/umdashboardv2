7808597f06d1930e6daf4707f1705c9528fc9da4
Stefan <shadzhiev@eservicios.indra.es>
2025-08-24 17:01:10 +0300

    add spacing between in dev message and features

--- src/shadcn/form.tsx
+++ src/shadcn/form.tsx
@@ -10,7 +10,7 @@ export const Form = () => {
         <form>
             <div className="flex flex-col gap-6">
               <div className={`grid gap-3 ${disabledClasses}`}>
-                <Label htmlFor="email">Email(in dev)</Label>
+                <Label htmlFor="email">Email (in dev)</Label>
                 <Input
                   id="email"
                   type="email"
@@ -20,7 +20,7 @@ export const Form = () => {
               </div>
               <div className={`grid gap-3 ${disabledClasses}`}>
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password(in dev)</Label>
+                  <Label htmlFor="password">Password (in dev)</Label>
                   <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
@@ -32,7 +32,7 @@ export const Form = () => {
               </div>
               <div className="flex flex-col gap-2">
                 <Button type="submit" className={`w-full bg-inherit text-black border-1 ${disabledClasses}`}>
-                  Login(in dev)
+                  Login (in dev)
                 </Button>
                 <ProviderLoginButton provider={PROVIDERS.Facebook} />
                 <div className="flex gap-2">
@@ -45,7 +45,7 @@ export const Form = () => {
             <div className={`mt-4 text-center text-sm ${disabledClasses}`}>
               Don&apos;t have an account?{" "}
               <a href="#" className="underline underline-offset-4">
-                Sign up(in dev)
+                Sign up (in dev)
               </a>
             </div>
           </form>
